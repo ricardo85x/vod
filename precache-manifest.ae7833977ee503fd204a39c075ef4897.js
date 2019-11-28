69b05c6f0f704a5bfeeef6a59f23f83fc69bc1a3
Ricardo <ricardoinliner@yahoo.com>
2019-11-28 20:35:54 -0300

    Updates

---
+++ precache-manifest.ae7833977ee503fd204a39c075ef4897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c3c97194a159cc1be26d0761a17f59",
+    "url": "/vod/index.html"
+  },
+  {
+    "revision": "0cf551ba61d38b1aee08",
+    "url": "/vod/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "62a2e68bea26af5c3815",
+    "url": "/vod/static/js/2.1a2edbf0.chunk.js"
+  },
+  {
+    "revision": "0cf551ba61d38b1aee08",
+    "url": "/vod/static/js/main.589ac878.chunk.js"
+  },
+  {
+    "revision": "a212e124144eb769eb75",
+    "url": "/vod/static/js/runtime-main.2d483a5c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vod/static/media/logo.25bf045c.svg"
+  }
+]);
